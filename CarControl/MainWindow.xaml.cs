c8fd1e98d1e457315ed043a36ab1206fccf9f968
Pedro William 🎮🎶 <axlwilliamrose5@hotmail.com>
2023-09-15 09:33:17 -0300

    Ajustes na UI, permitindo agora o redimensionamento.

--- CarControl/MainWindow.xaml.cs
+++ CarControl/MainWindow.xaml.cs
@@ -172,5 +172,20 @@ namespace CarControl
                 FormasPagtoWindowBtn_Click(sender, e);
             }
         }
+
+        private void MetroWindow_StateChanged(object sender, EventArgs e)
+        {
+            CenterWindowOnScreen();
+        }
+
+        private void CenterWindowOnScreen()
+        {
+            double screenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            double screenHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            double windowWidth = this.Width;
+            double windowHeight = this.Height;
+            this.Left = (screenWidth / 2) - (windowWidth / 2);
+            this.Top = (screenHeight / 2) - (windowHeight / 2);
+        }
     }
 }
