f0be2946547670fa48fc999261330b34d21af7a6
Pedro William 🎮🎶 <axlwilliamrose5@hotmail.com>
2023-09-15 09:38:10 -0300

    Ajustes no código

--- CarControl/MainWindow.xaml.cs
+++ CarControl/MainWindow.xaml.cs
@@ -180,12 +180,12 @@ namespace CarControl
 
         private void CenterWindowOnScreen()
         {
-            double screenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
-            double screenHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double screenHeight = SystemParameters.PrimaryScreenHeight;
             double windowWidth = this.Width;
             double windowHeight = this.Height;
-            this.Left = (screenWidth / 2) - (windowWidth / 2);
-            this.Top = (screenHeight / 2) - (windowHeight / 2);
+            Left = (screenWidth / 2) - (windowWidth / 2);
+            Top = (screenHeight / 2) - (windowHeight / 2);
         }
     }
 }
